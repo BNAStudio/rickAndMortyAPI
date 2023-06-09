e9a9af46d77e7d03efe5c3ab0e568f0fddf0eb2a
Daniel Marcovich <danielmark999@gmail.com>
2023-06-09 16:49:15 -0500

    feat(components): general components update

--- src/Components/Cards/Card.jsx
+++ src/Components/Cards/Card.jsx
@@ -1,10 +1,45 @@
 /* eslint-disable react/prop-types */
+import { Link } from 'react-router-dom'
 import css from './Card.module.css'
 
-const Card = ({ data }) => {
-    // console.log(data);
+const renderingEpisodes = (episode) => {
     return (
-        <div className={css.card}>{data}</div>
+        <>
+            {
+                episode && episode.map((item, i) => (
+                    <div key={`episode-${i + 1}`}>
+                        <Link to={item}>{`Episode ${i + 1}`}</Link>
+                    </div>
+                )
+                )
+            }
+
+        </>
+    )
+}
+
+const Card = ({ data }) => {
+    const { name, status, species, image, episode } = data
+    return (
+        <div className={css.card}>
+            <img src={image} className={css["image"]} />
+
+            <div className={css["character-info-container"]}>
+                <h3 className={css["character-name"]}>{name}</h3>
+
+                <div className={css.general}>
+                    <p className={css["info-character"]}><b>Status:</b> {status}</p>
+                    <p className={css["info-character"]}><b>Specie:</b> {species}</p>
+                </div>
+                <div className={css["episodes-list"]}>
+                    <p className={css["info-character"]}><b>Episodes:</b></p>
+                    <ol>
+                        {renderingEpisodes(episode)}
+                    </ol>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
--- src/Components/Characters/Characters.jsx
+++ src/Components/Characters/Characters.jsx
@@ -1,4 +1,3 @@
-import { useMemo } from 'react'
 import { useApiCall } from '../../Hooks/useApiCall'
 import Card from '../Cards/Card'
 import GridCard from '../Cards/GridCard'
@@ -8,19 +7,20 @@ const CHAR_URL = "https://rickandmortyapi.com/api/character"
 
 const Characters = () => {
 
-    const { dataInfo, loading } = useApiCall(CHAR_URL);
-
-    const data = useMemo(() => {
-        return dataInfo
-    }, [dataInfo])
-
-    console.log(data.results);
+    const { dataInfo } = useApiCall(CHAR_URL);
+    const data = dataInfo.results
 
     return (
         <div className={css["characters-container"]}>
-            <GridCard>
-                <Card data="online" />
-            </GridCard>
+            <>
+                <GridCard>
+                    {
+                        data && data.map(item => (
+                            <Card key={item.id} data={item} />
+                        ))
+                    }
+                </GridCard>
+            </>
         </div>
     )
 }
